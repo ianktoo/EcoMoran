01fc701084956ba47be43358ac74cd333d9c1834
Ian Too <ianktoo@gmail.com>
2021-11-18 22:38:07 +0300

    Added OG Metadata on Index Page

--- index.php
+++ index.php
@@ -10,6 +10,11 @@ $currentPage = "Home";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta property="og:title" content="EcoMoran : Home" />
+    <meta property="og:site_name" content="EcoMoran" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url"content ="https://ecomoran.org/"/>
     
     <!-- css -->
     <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
