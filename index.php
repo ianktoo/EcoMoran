44463c378adffe5da6d420e2abb93c24890c7c09
Ian Too <ianktoo@gmail.com>
2021-11-18 14:56:23 +0300

    Major Front-end Update

--- index.php
+++ index.php
@@ -18,6 +18,9 @@ $currentPage = "Home";
     <!-- Custom CSS -->
     <link rel="stylesheet" href="./css/standard.css">
 
+    <!-- Index Page CSS-->
+    <link rel="stylesheet" href="./css/index.css">
+
     <link rel="stylesheet" href="./node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css">
 
     <link rel="stylesheet" href="./node_modules/@splidejs/splide/dist/css/splide.min.css">
@@ -41,10 +44,105 @@ $currentPage = "Home";
 <!-- Defaults -->
 
 <!-- Hero Page section-->
+<section id="landing-page-header">
+  <div class="py-5" >
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8">
+          <h1>Eco Moran: <br> 
+            Sustainable Education for Children
+          </h1>
+          <p>The Future is Bright Green</p>
+          <p><a href="https://ecomoran.wixsite.com/ecomoran" class="btn btn-outline-light">LEARN MORE</a></p>
+        </div>
+
+        <div class="col-sm-8">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+<!-- Vision and Mission -->
+<section id="landing-page-vision-mission">
+<div class="container">
+  <div class="row p-4">
+    <div class="col-sm-6">
+      <h4>Our Mission</h4>
+      <p>
+        To equip children with attitudes, values, knowledge, and 
+        skills necessary to rethink and change current and future 
+        patterns of action in order to ensure healthy, environmentally 
+        just and sustainable futures for all.
+      </p>
+    </div>
+
+    <div class="col-sm-6">
+      <h4>Our Vision</h4>
+      <p>
+      To achieve the goal of global sustainability and foster global 
+      citizenship by positively influencing, shaping and teaching 
+      tomorrow’s leaders, professionals, and innovators.
+      </p>
+    </div>
+  </div>
+</div>
+</section>
+
+
+
+<section id="landing-page-social-media">
+  <div class="container">
+    <div class="d-flex flex-column justify-content-center align-items-center text-center p-5">
+      <h4 class="p-3">You can follow our stories on Social Media!</h4>
+      <p><a href="https://www.instagram.com/_ecomoran/" target="_blank" class="btn btn-outline-dark"> <i class="bi bi-instagram"></i> Follow Us</a></p>
+    </div>
+  </div>
+</section>
+
+
+
+
+<section id="landing-page-learn">
+  <div class="container-fluid">
+
+  <div class="text-center pt-4 pb-3">
+    <h4>Our curriculum</h4>
+  </div>
+    <div class="d-flex flex-wrap align-items-center justify-content-evenly p-3" id="card-box-learn" style="min-height:inherit;">
+      <div class="border shadow-sm  m-2 p-3">
+        <h4>Knowledge</h4>
+        <p class="lead">Our Curriculum </p>
+        <p>
+        Provide the children with new  insight and knowledge on different elements of the environment and nature.
+        </p>
+      </div>
+
+      <div class=" border shadow-sm  m-2 p-3">
+        
+        <h4>Skills</h4>
+        <p class="lead">Our Curriculum </p>
+        <p>
+        Allow the children to learn new skills that can contribute to environmental as well as their own personal well-being.
+        </p>
+      </div>
+
+      <div class=" border shadow-sm  m-2 p-3">
+        <h4>The Organic Garden</h4>
+        <p class="lead">Hands-on Activities </p>
+        <p>We have set up an organic/kitchen garden in the establishment we are working at where the children have the responsibility to take care of the garden. We will be setting up a kitchen garden in each establishment we engage with.  </p>
+      </div>
+    </div>
+  </div>
+</section>
 
 <!-- notices -->
 <?php include 'includes/default-notice.php';?>
 <!-- notices -->
+
 <!-- Home Page -->
 
 <!-- Footer -->
@@ -56,14 +154,15 @@ $currentPage = "Home";
 <script src="./node_modules/@splidejs/splide/dist/js/splide.min.js"></script>
 
 <script>
-  document.addEventListener( 'DOMContentLoaded', function() {
+  /* document.addEventListener( 'DOMContentLoaded', function() {
     var splide = new Splide( '.splide', {
       width : '100vw',
 		  height: '80vh',
       cover: true,
     } );
     splide.mount();
-  } );
+  } );*/
+
 </script>
 
 </body>
