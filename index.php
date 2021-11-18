9438031fd9e651d14eb79bd8b2a579a0c5f9d481
Ian Too <ianktoo@gmail.com>
2021-11-18 15:06:16 +0300

    Minor UI changes + addition of image assets

--- index.php
+++ index.php
@@ -97,6 +97,7 @@ $currentPage = "Home";
 <section id="landing-page-social-media">
   <div class="container">
     <div class="d-flex flex-column justify-content-center align-items-center text-center p-5">
+      <p class="text-muted lead">#EcoMoran</p>
       <h4 class="p-3">You can follow our stories on Social Media!</h4>
       <p><a href="https://www.instagram.com/_ecomoran/" target="_blank" class="btn btn-outline-dark"> <i class="bi bi-instagram"></i> Follow Us</a></p>
     </div>
@@ -110,15 +111,16 @@ $currentPage = "Home";
   <div class="container-fluid">
 
   <div class="text-center pt-4 pb-3">
-    <h4>Our curriculum</h4>
+    <h4 class="text-muted">#Our curriculum</h4>
   </div>
     <div class="d-flex flex-wrap align-items-center justify-content-evenly p-3" id="card-box-learn" style="min-height:inherit;">
-      <div class="border shadow-sm  m-2 p-3">
+      <div class="border shadow-sm m-2 p-3">
         <h4>Knowledge</h4>
         <p class="lead">Our Curriculum </p>
         <p>
         Provide the children with new  insight and knowledge on different elements of the environment and nature.
         </p>
+        <p><a href="https://ecomoran.wixsite.com/ecomoran/our-curriculum" class="container-fluid">Show Me How</a></p>
       </div>
 
       <div class=" border shadow-sm  m-2 p-3">
